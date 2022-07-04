9d105bc6ef37d520c062f17328866ca923b92bd2
Valerio Duchi <valerioduchi@gmail.com>
2022-07-04 12:56:19 +0200

    First

---
+++ helper/helper_web.py
@@ -0,0 +1,7 @@
+from os import *
+from selenium import webdriver
+from helper.helper_base import HelperFunc
+ 
+def get_browser(browser):
+    if browser == "chrome":
+        return HelperFunc(webdriver.Chrome())
