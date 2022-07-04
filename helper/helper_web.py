1de6bb961ecfd01e095b6f6b1f5fc856de1156a5
Valerio Duchi <valerioduchi@gmail.com>
2022-07-04 15:23:26 +0200

    Deleted old files

--- helper/helper_web.py
+++
@@ -1,7 +0,0 @@
-from os import *
-from selenium import webdriver
-from helper.helper_base import HelperFunc
- 
-def get_browser(browser):
-    if browser == "chrome":
-        return HelperFunc(webdriver.Chrome())
